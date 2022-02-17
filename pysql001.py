1454488599848b11b94b58dfd4f493b2ccbbac31
Babu <babu_sas@outlook.com>
2022-02-17 08:54:39 -0500

    Basic analysis-1

--- pysql001.py
+++ pysql001.py
@@ -11,7 +11,19 @@ try:
    df_orders = pd.read_sql(orders_sql, engine)
    details_sql = """SELECT * FROM details""";
    df_details = pd.read_sql(details_sql, engine)
-   print(df_orders) 
-   print(df_details) 
+   #print(df_orders) 
+   #print(df_details) 
+
+   ''' merge the data frames with common column'''
+   df_orders_details = df_orders.merge(df_details)
+   #Adding column for total sales
+   df_orders_details['total'] = df_orders_details.price * df_orders_details.quantity * (1 - df_orders_details.discount/100)
+   #Calculating discount
+   df_orders_details['off'] = df_orders_details.price * df_orders_details.quantity * (df_orders_details.discount/100)
+   #calcylating sales
+   df_sales = df_orders_details[['ordate','empl', 'total', 'off']]
+   ##Rounding off
+   df_orders_details = df_orders_details.round(2)
+   print(df_sales)
 except SQLAlchemyError as e:
    print(e)
