27f33f7a37a6a1858e79b06fdfcc91ef8dee7a58
Babu <babu_sas@outlook.com>
2022-02-17 19:36:47 -0500

    Pysql001 as SQL

---
+++ pysql002.py
@@ -0,0 +1,21 @@
+
+import pandas as pd
+import cx_Oracle
+import sys
+import sqlalchemy
+from sqlalchemy.exc import SQLAlchemyError
+lib_dir = r"C:\instantclient_19_13"
+cx_Oracle.init_oracle_client(lib_dir=lib_dir)
+try:
+   engine = sqlalchemy.create_engine("oracle+cx_oracle://pysql:pysql@localhost:1521/?service_name=xepdb1", arraysize=1000)
+   orders_sql = """SELECT  ordate, empl, 
+   price*quantity*(1-discount/100) AS total,
+    price*quantity*(discount/100) AS off
+    FROM orders INNER JOIN details 
+    ON orders.pono = details.pono""";
+   df_orders = pd.read_sql(orders_sql, engine)
+   print(df_orders);
+
+except SQLAlchemyError as e:
+   print(e)
+sys.exit();
